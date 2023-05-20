6dbbaead18fc26273ac82045854be2c24d8b3161
Enrique Román García Espino <70731354+ZorvShadow@users.noreply.github.com>
2023-05-20 16:32:35 -0600

    creo q ya está

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,13 @@
 import adapter from "@sveltejs/adapter-static";
 
+const dev = process.argv.includes("dev");
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "" : "",
-    },
-    prerender: {
-      handleHttpError: ({ path, referrer, message }) => {
-        // ignore deliberate link to shiny 404 page
-        if (
-          path === "/not-found" &&
-          referrer === "/blog/how-we-built-our-404-page"
-        ) {
-          return;
-        }
-
-        // otherwise fail the build
-        return undefined;
-      },
+      base: dev ? "" : "",
     },
   },
 };
