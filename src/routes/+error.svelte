fb39e7e4c84638f32571104f32f6c407984336c9
Enrique Román García Espino <70731354+ZorvShadow@users.noreply.github.com>
2023-05-20 22:22:12 -0600

    xdxdxd

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,33 +3,6 @@
   import DefaultLayout from "/src/layouts/DefaultLayout.svelte";
 </script>
 
-<DefaultLayout title="Error en la página">
-  <main class="mainpart">
-    <div class="indicebox">
-      <ul class="indice">
-        <!-- <li>El índice de la página será autogenerado por el javascript dentro de /js/blog.js</li> -->
-      </ul>
-    </div>
-    <article id="mainarticle">
-      <!--Es importante que estos contenidos se encuentren dentro de la etiqueta article, y con el id main article, usualmente solo habrá uno de estos por cada página, debido a que contienen un h1, no es recomendado utilizarlo más de una vez-->
-
-      <h1 class="centrar">
-        Error 404: Página no encontrada
-        <!--Insertar el título de la página en general, por ejemplo: "Actividades de Álgebra"-->
-      </h1>
-      <!--Para la correcta visualización del índice, deberá de estar separada cada sección con un h2, recuerda iniciar con un h1 para marcar la jerarquía de la página (es muy importante para los lectores de pantalla o un visualizador en modo lectura como el de Safari)-->
-      <h2>
-        ¿Qué Puedes Hacer?
-        <!--Dentro de este, iran los subitulos del contenido, ejemplo: "Exámenes parciales"-->
-      </h2>
-      <p class="justificar samesubtopic">
-        Si crees que es un error favor de contactar a cualquiera de los
-        creadores del sitio.
-        <!--Existen diversas clases para organizar el parrafo, una de ellas es "justificar", como su nombre lo dice, justifica el texto dentro del párrafo-->
-        <!--Si el párrafo no es el último, agregale la clase "samesubtopic", esta remueve los padding para que se sienta continuo el documento-->
-
-        <!--Aquí el contenido o descripción de las actividades, intentaré hacer un div para poner imagenes, etc.-->
-      </p>
-    </article>
-  </main>
-</DefaultLayout>
+<svelte:head>
+  <meta http-equiv="refresh" content="0; url=/404" />
+</svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,73 +1,85 @@
 <!-- src/routes/Home.svelte -->
 <script>
-	import DefaultLayout from '/src/layouts/DefaultLayout.svelte';
+  import DefaultLayout from "/src/layouts/DefaultLayout.svelte";
 </script>
 
 <DefaultLayout title="Página de inicio">
-	<main class="mainpart">
-		<div class="indicebox">
-			<ul class="indice" />
-		</div>
-		<article id="mainarticle">
-			<h1 class="centrar">Bienvenido al Compendio Bátiz</h1>
-			<h2>Introducción</h2>
-			<p class="justificar samesubtopic">
-				Hola ¿qué tal?. Hace un tiempo, junto a un amigo, decidímos crear este sitio, para compartir
-				algunos recursos y materiales para <em> llevarsela </em> más fácil en su estancia en Bátiz, puesto
-				que las cosas se puedenen complicar seguido y bastante.
-			</p>
+  <main class="mainpart">
+    <div class="indicebox">
+      <ul class="indice" />
+    </div>
+    <article id="mainarticle">
+      <h1 class="centrar">Bienvenido al Compendio Bátiz</h1>
+      <h2>Introducción</h2>
+      <p class="justificar samesubtopic">
+        Hola ¿qué tal?. Hace un tiempo, junto a un amigo, decidímos crear este
+        sitio, para compartir algunos recursos y materiales para <em>
+          llevarsela
+        </em> más fácil en su estancia en Bátiz, puesto que las cosas se puedenen
+        complicar seguido y bastante.
+      </p>
 
-			<p class="justificar samesubtopic">
-				En estos espacios podrás encontrar apuntes, resúmenes, exámenes y otros materiales que hemos
-				recopilado durante nuestra estancia en la escuela, además de materiales donados por
-				compañeros y excompañeros. Estos recursos nos han ayudado a comprender mejor algunos temas,
-				y esperamos que también puedan ser útiles para ustedes.
-			</p>
+      <p class="justificar samesubtopic">
+        En estos espacios podrás encontrar apuntes, resúmenes, exámenes y otros
+        materiales que hemos recopilado durante nuestra estancia en la escuela,
+        además de materiales donados por compañeros y excompañeros. Estos
+        recursos nos han ayudado a comprender mejor algunos temas, y esperamos
+        que también puedan ser útiles para ustedes.
+      </p>
 
-			<p class="justificar">Apreciariamos mucho cualquier aporte de material o sugerencia.</p>
+      <p class="justificar">
+        Apreciariamos mucho cualquier aporte de material o sugerencia.
+      </p>
 
-			<h2>¿Cómo navegar dentro de la página?</h2>
-			<p class="justificar">
-				El compendio esta hecho para que se pueda navegar en cualquier dispositivo, ya sea móvil,
-				computadora o laptob (si tu chingadera es de la era del caldo obvio no va a correr XD)
-			</p>
-			<p class="justificar">
-				La navegación en ambos tipos de dispositivos es bastante sencilla de reconocer y utilizar.
-				En teléfonos celulares se encuentra un menú desplegable en la parte superior derecha que, al
-				hacer clic se mostrarán los seis semestres y al hacer clic en cada semestre saldrán sus
-				respectivas unidades de aprendizaje. Por otro lado, en las computadoras y laptobs la forma
-				de navegación es más sencilla de ver, ya que los semestres se encuentran en la parte
-				superior de la página y al pasar el cursor sobre ellos, se mostrarán cada una de las
-				unidades con su respectivo semestre.
-			</p>
-			<p class="justificar">
-				No olvides contactar a alguno de los creadores del sitio si encuentras un error al navegar
-				dentro del compendio.
-			</p>
-			<h2>¿Por qué?</h2>
-			<p class="justificar">pq no uwu</p>
-			<h2>¿xd?</h2>
-			<p class="justificar">xd</p>
-			<h2>Aenean quis efficitur nisl</h2>
-			<p class="justificar">
-				Sed ultrices, urna sit amet mollis posuere, mi tellus ornare tellus, eget luctus eros nibh
-				in mauris. Proin nunc massa, vestibulum vel interdum id, pulvinar sit amet nisi. In congue
-				sodales tempus. Nam velit magna, cursus in iaculis id, consequat sagittis sem. Integer sed
-				rutrum tellus. Sed vitae placerat orci. Fusce ultricies massa ut mi tristique lacinia.
-				Mauris bibendum risus non lacus ultricies varius.
-			</p>
-			<h2>Pellentesque in sem euismod</h2>
-			<p class="justificar">
-				Pellentesque in sem euismod, egestas neque vel, finibus felis. Mauris ultrices vestibulum
-				augue non pharetra. Fusce tristique lectus sit amet ex eleifend malesuada eget sed nibh.
-				Maecenas tempor faucibus pellentesque. Nullam varius condimentum nunc non auctor. Nulla
-				blandit, dui eu auctor porta, ante nisl venenatis nunc, vitae tincidunt nibh nulla in leo.
-				Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi et nunc feugiat justo
-				imperdiet euismod auctor a ligula. Phasellus vel arcu vel diam semper aliquet. Lorem ipsum
-				dolor sit amet, consectetur adipiscing elit. Aliquam a vestibulum tortor, at consequat mi.
-			</p>
-			<h2>si</h2>
-			<p class="justificar">si</p>
-		</article>
-	</main>
+      <h2>¿Cómo navegar dentro de la página?</h2>
+      <p class="justificar">
+        El compendio esta hecho para que se pueda navegar en cualquier
+        dispositivo, ya sea móvil, computadora o laptob (si tu chingadera es de
+        la era del caldo obvio no va a correr XD)
+      </p>
+      <p class="justificar">
+        La navegación en ambos tipos de dispositivos es bastante sencilla de
+        reconocer y utilizar. En teléfonos celulares se encuentra un menú
+        desplegable en la parte superior derecha que, al hacer clic se mostrarán
+        los seis semestres y al hacer clic en cada semestre saldrán sus
+        respectivas unidades de aprendizaje. Por otro lado, en las computadoras
+        y laptobs la forma de navegación es más sencilla de ver, ya que los
+        semestres se encuentran en la parte superior de la página y al pasar el
+        cursor sobre ellos, se mostrarán cada una de las unidades con su
+        respectivo semestre.
+      </p>
+      <p class="justificar">
+        No olvides contactar a alguno de los creadores del sitio si encuentras
+        un error al navegar dentro del compendio.
+      </p>
+      <h2>¿Por qué?</h2>
+      <p class="justificar">pq no uwu</p>
+      <h2>¿xd?</h2>
+      <p class="justificar">xd</p>
+      <h2>Aenean quis efficitur nisl</h2>
+      <p class="justificar">
+        Sed ultrices, urna sit amet mollis posuere, mi tellus ornare tellus,
+        eget luctus eros nibh in mauris. Proin nunc massa, vestibulum vel
+        interdum id, pulvinar sit amet nisi. In congue sodales tempus. Nam velit
+        magna, cursus in iaculis id, consequat sagittis sem. Integer sed rutrum
+        tellus. Sed vitae placerat orci. Fusce ultricies massa ut mi tristique
+        lacinia. Mauris bibendum risus non lacus ultricies varius.
+      </p>
+      <h2>Pellentesque in sem euismod</h2>
+      <p class="justificar">
+        Pellentesque in sem euismod, egestas neque vel, finibus felis. Mauris
+        ultrices vestibulum augue non pharetra. Fusce tristique lectus sit amet
+        ex eleifend malesuada eget sed nibh. Maecenas tempor faucibus
+        pellentesque. Nullam varius condimentum nunc non auctor. Nulla blandit,
+        dui eu auctor porta, ante nisl venenatis nunc, vitae tincidunt nibh
+        nulla in leo. Interdum et malesuada fames ac ante ipsum primis in
+        faucibus. Morbi et nunc feugiat justo imperdiet euismod auctor a ligula.
+        Phasellus vel arcu vel diam semper aliquet. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Aliquam a vestibulum tortor, at consequat
+        mi.
+      </p>
+      <h2>si</h2>
+      <p class="justificar">si</p>
+    </article>
+  </main>
 </DefaultLayout>
