24abf2c089d50e4e701311748c207e3367af3fb7
Quique <progamerpooprabah@gmail.com>
2023-06-21 22:01:21 -0600

    Intenté el pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
+
 import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
@@ -7,6 +9,7 @@ export default defineConfig({
 		sveltekit(),
 		Icons({
 			compiler: 'svelte'
-		})
+		}),
+		SvelteKitPWA()
 	]
 })
