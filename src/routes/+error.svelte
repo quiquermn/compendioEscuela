e7bf5e9a8b5972b952be94f8996b162ecb273a4c
Enrique Román García Espino <quique@quiqueso.com>
2023-07-13 14:00:39 -0600

    Test 2

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,5 @@
 <article>
-	<h1>Error 404: Página no encontrada</h1>
+	<h1>Error 404: Página no encontrada 🤓👆</h1>
 	<h2>¿Qué Puedes Hacer?</h2>
 	<p>
 		Si crees que es un error favor de contactar a cualquiera de los creadores del sitio mostrados en
@@ -7,6 +7,6 @@
 	</p>
 	<p class="text-center">
 		(Las páginas no mostradas siguen en desarrollo, pronto podrás consultarlas, agradecemos su
-		comprensión y paciencia)🤓👆.
+		comprensión y paciencia).
 	</p>
 </article>
