eac8fae597118e5e0989b099d4bda15bf8a2ce99
Kaleb <kaleborojas@gmail.com>
2023-09-20 19:38:54 -0600

    quimica y fisica

---
+++ src/routes/semestres/3tercero/quimica1/+page.svelte
@@ -0,0 +1,21 @@
+<article>
+	<h1>Química</h1>
+	<section>
+		<h2>Breve Introducción (En desarrollo)</h2>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, quidem dignissimos? Laborum
+			ullam temporibus ipsa sunt aliquam obcaecati ipsam recusandae, harum, similique iure, nemo
+			corrupti dignissimos eaque modi voluptate dolorum.
+		</p>
+	</section>
+	<section>
+		<h2>Primer Parcial (En desarrollo)</h2>
+	</section>
+	<section>
+		<h2>Segundo Parcial (En desarrollo)</h2>
+	</section>
+	<section>
+		<h2>Tercer Parcial</h2>
+		<h3>Temas a tratar</h3>
+	</section>
+</article>
