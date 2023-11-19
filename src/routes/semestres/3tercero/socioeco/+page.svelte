bc74115b84acaeff6f4422d7e0899dd3c5492a2c
Kaleb <kaleborojas@gmail.com>
2023-11-19 00:21:18 -0600

    entorno intro

---
+++ src/routes/semestres/3tercero/socioeco/+page.svelte
@@ -0,0 +1,71 @@
+<article>
+	<h1>Entorno Socioeconómico de México</h1>
+	<section>
+		<h2>Breve Introducción (En desarrollo)</h2>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, quidem dignissimos? Laborum
+			ullam temporibus ipsa sunt aliquam obcaecati ipsam recusandae, harum, similique iure, nemo
+			corrupti dignissimos eaque modi voluptate dolorum.
+		</p>
+	</section>
+	<section>
+		<h2>Primer Parcial (En desarrollo)</h2>
+	</section>
+	<section>
+		<h2>Segundo Parcial (En desarrollo)</h2>
+	</section>
+	<section>
+		<h2>Tercer Parcial</h2>
+		<h3>Temas a tratar</h3>
+		<p>
+			De acuerdo al "Cronograma para el Tercer Departamental" del 2023, se trataran los siguientes
+			temas:
+		</p>
+		<strong> RAP 1:</strong>
+		<ol>
+			<li>Identifica el proceso de la Gl.</li>
+			<li>Describir ventajas y desventajas de la Globalización.</li>
+		</ol>
+	</section>
+	<section>
+		<strong> RAP 2:</strong>
+		<ol>
+			<li>Impacto en la sociedad de las principales Organizaciones Sociales y los Partidos Políticos.</li>
+			<li>Derechos Humanos y la participación de las Organizaciones Sociales en México (Grupos de presión, ONG's).</li>
+		</ol>
+	</section>
+	<h3>Globalización</h3>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum molestiae assumenda eum repellat quod modi cumque totam, dicta nam natus! Exercitationem quis dolorum voluptatibus dolor nobis rerum ex beatae iure.
+	</p>
+	<p>
+		<strong>Características:</strong>
+	</p>
+	<ul>
+		<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, asperiores at culpa similique voluptate maxime! Voluptatibus, dolore hic? Libero harum quia eveniet quam eaque omnis laboriosam voluptates esse cum vitae?
+		</li>
+		<li>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita aperiam quia itaque illo laboriosam sapiente nulla, distinctio recusandae voluptatum delectus necessitatibus velit beatae placeat quaerat? Deserunt quos est harum voluptatum!
+		</li>
+	</ul>
+	<p>
+		<strong>Ventajas:</strong>
+	</p>
+	<ul>
+		<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, asperiores at culpa similique voluptate maxime! Voluptatibus, dolore hic? Libero harum quia eveniet quam eaque omnis laboriosam voluptates esse cum vitae?
+		</li>
+		<li>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita aperiam quia itaque illo laboriosam sapiente nulla, distinctio recusandae voluptatum delectus necessitatibus velit beatae placeat quaerat? Deserunt quos est harum voluptatum!
+		</li>	</ul>
+		
+	<p>
+		<strong>Desventajas</strong>
+	</p>
+	<ul>
+		<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, asperiores at culpa similique voluptate maxime! Voluptatibus, dolore hic? Libero harum quia eveniet quam eaque omnis laboriosam voluptates esse cum vitae?
+		</li>
+		<li>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita aperiam quia itaque illo laboriosam sapiente nulla, distinctio recusandae voluptatum delectus necessitatibus velit beatae placeat quaerat? Deserunt quos est harum voluptatum!
+		</li>
+	
+</article>
