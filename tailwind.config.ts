521a2306436e955b571a5fd6befe75adfeda839f
Enrique Román García Espino <70731354+ZorvShadow@users.noreply.github.com>
2023-12-25 04:27:54 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'tailwindcss'
 import forms from '@tailwindcss/forms'
 import typography from '@tailwindcss/typography'
 import { skeleton } from '@skeletonlabs/tw-plugin'
+import { myCustomTheme } from './custom-theme'
 
 export default {
 	darkMode: 'class',
@@ -31,6 +32,9 @@ export default {
 						name: 'skeleton',
 						enhancements: true
 					}
+				],
+			custom: [
+						myCustomTheme
 				]
 			}
 		})
